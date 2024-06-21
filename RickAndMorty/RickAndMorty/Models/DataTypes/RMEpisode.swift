38a686d0899777f3e2f18a45b81201c0dfcebef6
Ivan Semenov <izzuyinvest@gmail.com>
2024-06-21 22:12:33 +0300

    Fetch Episodes

--- RickAndMorty/RickAndMorty/Models/DataTypes/RMEpisode.swift
+++ RickAndMorty/RickAndMorty/Models/DataTypes/RMEpisode.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 // MARK: - RMEpisode
-struct RMEpisode: Codable {
+struct RMEpisode: Codable, RMEpisodeDataRender {
     let id: Int
     let name: String
-    let airDate: String     //air_date
+    let air_date: String     
     let episode: String
     let characters: [String]
     let url: String
