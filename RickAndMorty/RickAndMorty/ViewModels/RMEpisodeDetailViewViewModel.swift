34a9aed872f5b8d8433f59521b3b7c6271a57959
Ivan Semenov <izzuyinvest@gmail.com>
2024-06-23 17:06:50 +0300

    API Cach Layer

---
+++ RickAndMorty/RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  RMEpisodeDetailViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Иван Семенов on 23.06.2024.
+//
+
+import UIKit
+
+class RMEpisodeDetailViewViewModel {
+    private let endpointUrl: URL?
+    
+    init(endpointUrl: URL?) {
+        self.endpointUrl = endpointUrl
+        fetchEpisodeData()
+    }
+    
+    private func fetchEpisodeData() {
+        guard let url = endpointUrl,
+              let request = RMRequest(url: url) else {
+            return
+        }
+        RMService.shared.execute(request, expecting: RMEpisode.self) { result in
+            switch result {
+            case .success(let success):
+                print(String(describing: success))
+            case .failure(let failure):
+                break
+            }
+        }
+    }
+}
---
+++ RickAndMorty/RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  RMEpisodeDetailView.swift
+//  RickAndMorty
+//
+//  Created by Иван Семенов on 23.06.2024.
+//
+
+import UIKit
+
+final class RMEpisodeDetailView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+}
