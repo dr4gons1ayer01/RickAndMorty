4adf15b0fc28256d299793f4563683b61996aee7
Ivan Semenov <izzuyinvest@gmail.com>
2024-06-27 10:24:30 +0300

    Episode Detail View

--- RickAndMorty/RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
@@ -7,26 +7,77 @@
 
 import UIKit
 
-class RMEpisodeDetailViewViewModel {
+protocol RMEpisodeDetailViewViewModelDelegate: AnyObject {
+    func didFetchEpisodeDetails()
+}
+
+final class RMEpisodeDetailViewViewModel {
     private let endpointUrl: URL?
+    private var dataTuple: (RMEpisode, [RMCharacter])? {
+        didSet {
+            delegate?.didFetchEpisodeDetails()
+        }
+    }
+    public weak var delegate: RMEpisodeDetailViewViewModelDelegate?
+    
+    // MARK: - Init
     
     init(endpointUrl: URL?) {
         self.endpointUrl = endpointUrl
-        fetchEpisodeData()
+
     }
     
-    private func fetchEpisodeData() {
+    // MARK: - Public
+    
+    /// Fetch backing episode model
+    public func fetchEpisodeData() {
         guard let url = endpointUrl,
               let request = RMRequest(url: url) else {
             return
         }
-        RMService.shared.execute(request, expecting: RMEpisode.self) { result in
+        RMService.shared.execute(request, expecting: RMEpisode.self) { [weak self] result in
             switch result {
-            case .success(let success):
-                print(String(describing: success))
+            case .success(let model):
+                self?.fetchRelatedCharacters(episode: model)
             case .failure:
                 break
             }
+        }
+    }
+    
+    // MARK: - Private
+    
+    private func fetchRelatedCharacters(episode: RMEpisode) {
+        let charactersUrls: [URL] = episode.characters.compactMap {
+            return URL(string: $0)
+        }
+        let requests: [RMRequest] = charactersUrls.compactMap {
+            return RMRequest(url: $0)
+        }
+        // 10 of parallel requests
+        // Notified once all done
+        
+        let group = DispatchGroup()
+        var characters: [RMCharacter] = []
+        for request in requests {
+            group.enter()
+            RMService.shared.execute(request, expecting: RMCharacter.self) { result in
+                defer {
+                    group.leave()
+                }
+                switch result {
+                case .success(let model):
+                    characters.append(model)
+                case .failure:
+                    break
+                }
+            }
+        }
+        group.notify(queue: .main) {
+            self.dataTuple = (
+            episode,
+            characters
+            )
         }
     }
 }
--- RickAndMorty/RickAndMorty/Views/RMEpisodeDetailView.swift
+++ RickAndMorty/RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -8,15 +8,52 @@
 import UIKit
 
 final class RMEpisodeDetailView: UIView {
-
+    
+    private var viewModel: RMEpisodeDetailViewViewModel?
+    
+    private var collectionView: UICollectionView?
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    // MARK: - Init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .red
+//        self.collectionView = createCollectionView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("Unsupported")
     }
     
+    private func addConstraits() {
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+//    private func createCollectionView() -> UICollectionView{
+//        
+//    }
+    
+    // MARK: - Public
+    
+    public func configure(with viewModel: RMEpisodeDetailViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
 }
+
