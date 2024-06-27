ccf8b309dd53529cfa786c971a3fdb5ac4f8cf1b
Ivan Semenov <izzuyinvest@gmail.com>
2024-06-27 16:08:06 +0300

    Episode Detail Cells

--- RickAndMorty/RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeDetailViewViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeDetailViewViewModel.swift
@@ -13,8 +13,9 @@ protocol RMEpisodeDetailViewViewModelDelegate: AnyObject {
 
 final class RMEpisodeDetailViewViewModel {
     private let endpointUrl: URL?
-    private var dataTuple: (RMEpisode, [RMCharacter])? {
+    private var dataTuple: (episode: RMEpisode, characters: [RMCharacter])? {
         didSet {
+            createCellViewModels()
             delegate?.didFetchEpisodeDetails()
         }
     }
@@ -26,7 +27,7 @@ final class RMEpisodeDetailViewViewModel {
     
     public weak var delegate: RMEpisodeDetailViewViewModelDelegate?
     
-    public private(set) var sections: [SectionType] = []
+    public private(set) var cellViewModels: [SectionType] = []
     
     // MARK: - Init
     
@@ -54,6 +55,29 @@ final class RMEpisodeDetailViewViewModel {
     }
     
     // MARK: - Private
+    
+    private func createCellViewModels() {
+        guard let dataTuple = dataTuple else {
+            return
+        }
+        let episode = dataTuple.episode
+        let characters = dataTuple.characters
+        cellViewModels = [
+            .information(viewModel: [
+                .init(title: "Episode Name", value: episode.name),
+                .init(title: "Air Date", value: episode.air_date),
+                .init(title: "Episode", value: episode.episode),
+                .init(title: "Created", value: episode.created),
+            ]),
+            .characters(viewModel: characters.compactMap({ character in
+                return RMCharacterCollectionViewCellViewModel(
+                    characterName: character.name,
+                    characterStatus: character.status,
+                    characterImageUrl: URL(string: character.image)
+                )
+            }))
+        ]
+    }
     
     private func fetchRelatedCharacters(episode: RMEpisode) {
         let charactersUrls: [URL] = episode.characters.compactMap {
@@ -83,8 +107,8 @@ final class RMEpisodeDetailViewViewModel {
         }
         group.notify(queue: .main) {
             self.dataTuple = (
-            episode,
-            characters
+                episode: episode,
+                characters: characters
             )
         }
     }
--- RickAndMorty/RickAndMorty/Views/EpisodeDetails/RMEpisodeInfoCollectionViewCell.swift
+++ RickAndMorty/RickAndMorty/Views/EpisodeDetails/RMEpisodeInfoCollectionViewCell.swift
@@ -8,5 +8,32 @@
 import UIKit
 
 final class RMEpisodeInfoCollectionViewCell: UICollectionViewCell {
+    static let cellIdentifer = "RMEpisodeInfoCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .secondarySystemBackground
+        setUpLayer()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+    
+    private func setUpLayer() {
+        layer.cornerRadius = 8
+        layer.masksToBounds = true
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.secondaryLabel.cgColor
+    }
+    
+    func configure(with viewModel: RMEpisodeInfoCollectionViewCellViewModel) {
+        
+    }
     
 }
